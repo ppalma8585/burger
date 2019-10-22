dce23c095cb5afa8578d0b6dc5c8118d7e6934d5
Paul Palma <palmachato@yahoo.com>
2019-10-22 12:22:01 -0400

    changed name of table in seed file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
-INSERT INTO burgers_db (burger_name,devoured)
+INSERT INTO burgers (burger_name,devoured)
 VALUES ('Bacon', false);
 
-INSERT INTO burgers_db (burger_name,devoured)
+INSERT INTO burgers (burger_name,devoured)
 VALUES ('Chicken', false);
 
-INSERT INTO burgers_db (burger_name,devoured)
+INSERT INTO burgers (burger_name,devoured)
 VALUES ('Turkey', false);
