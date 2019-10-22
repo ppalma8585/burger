d2995a5af7b0a981d882330a2562933d227b2d5a
Paul Palma <palmachato@yahoo.com>
2019-10-22 08:30:55 -0400

    getting heroku to work

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,26 @@
 var mysql = require("mysql")
 
-var connection = mysql.createConnection({
-     host: "localhost",
-     port: 3306,
-     user: "root",
-     password: "root",
-     database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//      host: "localhost",
+//      port: 3306,
+//      user: "root",
+//      password: "root",
+//      database: "burgers_db"
+// });
 
-// make connection
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id" + connection.threadId)
- })
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+  } else {
+
+    var connection = mysql.createConnection({
+      port: 3306,
+      host: "localhost",
+      user: "root",
+      password: "root",
+      database: "burgers_db"
+    });
+  };
+
 
 //  export connection for our ORM to use
 module.exports = connection;
