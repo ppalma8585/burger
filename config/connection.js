a1fd18931532f410b5a7b8d4c81a72f10486121d
Paul Palma <palmachato@yahoo.com>
2019-10-22 08:55:32 -0400

    trying something else

--- config/connection.js
+++ config/connection.js
@@ -1,33 +1,60 @@
-var mysql = require("mysql")
+// var mysql = require("mysql")
 
-// var connection = mysql.createConnection({
-//      host: "localhost",
-//      port: 3306,
-//      user: "root",
-//      password: "root",
-//      database: "burgers_db"
-// });
+// // var connection = mysql.createConnection({
+// //      host: "localhost",
+// //      port: 3306,
+// //      user: "root",
+// //      password: "root",
+// //      database: "burgers_db"
+// // });
+
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL)
+//   } else {
+
+//     connection = mysql.createConnection({
+//       host: "localhost",
+//       user: "root",
+//       password: "root",
+//       database: "burgers_db"
+//     });
+//   };
+
+//   connection.connect(function (err) {
+//     if (err) {
+//       console.error('error connecting: ' + err.stack);
+//       return;
+//     }
+//     console.log('connected as id ' + connection.threadId);
+//   });
+
+
+// //  export connection for our ORM to use
+// module.exports = connection;
+
+/*
+Make the connection to the database and export to be used by the O.R.M.
+*/
+var mysql = require('mysql');
+var connection;
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL)
-  } else {
-
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
     connection = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "root",
-      database: "burgers_db"
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
     });
-  };
+};
 
-  connection.connect(function (err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-    console.log('connected as id ' + connection.threadId);
-  });
+connection.connect(function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
 
-
-//  export connection for our ORM to use
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,27 +1,59 @@
   
-var express = require("express");
+// var express = require("express");
 
-var port = process.env.PORT || 8080;
+// var port = process.env.PORT || 8080;
+// var app = express();
+
+// // Serve static content for the app from the "public" directory in the application directory.
+// app.use(express.static("public"));
+
+// // Parse application body as JSON
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // Set Handlebars.
+// var exphbs = require("express-handlebars");
+
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+// // Import routes and give the server access to them.
+// var routes = require("./controllers/burgers_controller.js");
+
+// app.use(routes);
+
+// app.listen(port, function() {
+//   console.log("App is listening on port " + port);
+// });
+
+/* Set up the server file to require npm packages, express middleware, body-parser, methodOverride, espress-handlebars and the handlebars engine
+*/
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
 var app = express();
+var router = require('./controllers/burgers_controller.js');
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+//setup handlebars templating engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+app.use('/', router);
+app.use(express.static("public"));
 
-app.use(routes);
-
+// Set up the server
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("App is listening on port " + port);
 });
